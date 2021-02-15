843ddba64efaa58075852d3a3866bc2774726b2b
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-15 16:39:20 -0500

    commit tree d4a4d431d3be46ae54605facef40cd399057cbfe

---
+++ lib/artist.rb
@@ -0,0 +1,50 @@
+# class Artist
+#    attr_accessor :name
+
+#     @@all = []
+
+#     def initialize(name)
+#         @name = name
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+#     def add_song(song)
+#        song.artist = self
+#     end
+
+#     def add_song_by_name(name)
+#         song = Song.new(name)
+#         song.artist = self
+#     end
+
+#     def songs
+#         Song.all.select {|song| song.artist == self}
+#     end
+
+#     def self.song_count
+#         Song.all.count
+#     end
+
+# end
+
+class Song
+    attr_accessor :name, :artist
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        artist.name if artist
+    end
+end
